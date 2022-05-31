8877c85f4231434c98fdb43d31afc3e9c04518f4
Lucas Kobayashi <kobacarmo2@hotmail.com>
2022-05-30 22:34:30 -0300

    Finalizando os componentes de opção de feedback

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -2,16 +2,22 @@ import React, { useRef } from "react";
 import { TouchableOpacity } from "react-native";
 import { ChatTeardropDots } from "phosphor-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
+import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 import { styles } from "./styles";
 import { theme } from "../../theme";
+import { Options } from "../Options";
 
-export function Widget() {
+function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand();
+  }
 
   return (
     <>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
         <ChatTeardropDots
           size={24}
           weight="bold"
@@ -19,7 +25,16 @@ export function Widget() {
         />
       </TouchableOpacity>
 
-      <BottomSheet ref={bottomSheetRef} snapPoints={[1, 280]}></BottomSheet>
+      <BottomSheet
+        ref={bottomSheetRef}
+        snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
+      >
+        <Options />
+      </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
